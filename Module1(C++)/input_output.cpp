88a1402617928de6a69a19ec14036171e1411214
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-05-20 23:21:59 +0600

    cpp input && output

---
+++ Module1(C++)/input_output.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main()
+{
+  cout<<"I am a Software Engineer"<<endl;
+  cout<<"hello"<<endl<<"world\n";
+  int a = 100, b = 200;
+  cout<<a<<" "<<b;
+  return 0;
+}
