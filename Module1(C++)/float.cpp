d71e253f39507d6f246b711d313f41cfa6410c05
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-05-20 23:51:23 +0600

    float value input

---
+++ Module1(C++)/float.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+  float a;
+  cin>>a;
+  cout<<fixed<<setprecision(2)<<a; // koto decimal print korte hobe
+  return 0;
+}
--- Module1(C++)/input_output.cpp
+++ Module1(C++)/input_output.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 main()
 {
+  int x, y;
+  cin>>x>>y; // c++ input nyer formate----
   cout<<"I am a Software Engineer"<<endl;
   cout<<"hello"<<endl<<"world\n";
   int a = 100, b = 200;
-  cout<<a<<" "<<b;
+  cout<<a<<" "<<b<<endl; // output dehar formate
+  cout<<x<<" "<<y;
   return 0;
 }
