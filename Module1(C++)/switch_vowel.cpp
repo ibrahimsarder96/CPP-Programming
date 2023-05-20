e16fb5dd11651d4a48c8d0f10189c7bddcd50306
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-05-21 00:37:58 +0600

    switch case

---
+++ Module1(C++)/switch_vowel.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+main()
+{
+  char a;
+  cin>>a;
+  switch(a)
+  {
+    case 'a':
+        cout<<"Vowel"<<endl;
+    break;
+    case 'e':
+        cout<<"Vowel"<<endl;
+    break;
+    case 'i':
+        cout<<"Vowel"<<endl;
+    break;
+    case 'o':
+        cout<<"Vowel"<<endl;
+    break;
+    case 'u':
+        cout<<"Vowel"<<endl;
+    break;
+    default:
+        cout<<"Constant"<<endl;
+  }
+  return 0;
+}
