f716709e89590c2540236d00958eca2b30980446
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-05-21 23:01:09 +0600

    dynamic memory allocated

---
+++ Module2(DynamicMemoryAllocation)/dynamic_heap.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+  // int *a = new int; // syntax dynamic memory----
+  // *a = 10;
+  // cout<<*a;
+  float *f = new float;
+  *f = 2.50;
+  cout<<*f;
+  return 0;
+}
