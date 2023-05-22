751208385dd79b7f8ba492f83dd6cc3126586b99
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-05-22 21:13:13 +0600

    tarnary operator

--- Module2(DynamicMemoryAllocation)/dynamic_array_size_increase.cpp
+++ Module2(DynamicMemoryAllocation)/dynamic_array_size_increase.cpp
@@ -3,24 +3,30 @@ using namespace std;
 main()
 {
   int n;
-  cin>>n;
-  int * a = new int[n];
+  cin>>n;       // first array size input
+  int * a = new int[n];    // first dynamic array
   for(int i = 0; i < n; i++)
   {
-    cin>>a[i];
+    cin>>a[i];    // first array input
   }
-  int size;
+  int size;   // second array size input
   cin>>size;
-  int * b = new int[size];
-  for(int i = 0; i < size; i++)
+  int * b = new int[size];    // second dynamic array
+  for(int i = 0; i < n; i++)
   {
-    b[i] = a[i];
+    b[i] = a[i];    // first array copy second array
   }
-  b[5] = 60;
+  b[5] = 60;    // second array value;
   b[6] = 70;
   for(int i = 0; i < size; i++)
   {
-    cout<<b[i]<<endl;
+    cout<<b[i]<<endl;     // second array print;
+  }
+  cout<<endl;
+  delete[] a;     // check first array delete;
+  for(int i = 0; i < n; i++)
+  {
+    cout<<a[i]<<endl;     // second array print;
   }
   return 0;
 }
---
+++ Module2(DynamicMemoryAllocation)/tarnary_oparetor.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+  int n = 10;
+  (n%2==0)?cout<<"even":cout<<"odd";
+  return 0;
+}
