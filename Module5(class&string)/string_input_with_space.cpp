a512f792a5bd2cf8e567afd54a3d4dc6128872a1
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-05-28 01:02:02 +0600

    stiring space

---
+++ Module5(class&string)/string_input_with_space.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+  int x;
+  cin>>x;
+  // cin.ignore();  // string er age integer input nile  ai function use korte hobe
+  getchar(); // string er age integer input nile  ai function use korte hobe
+  string s;
+  getline(cin,s); // stirng with space function --getline()
+  cout<<x<<endl;
+  cout<<s<<endl;
+
+  return 0;
+}
