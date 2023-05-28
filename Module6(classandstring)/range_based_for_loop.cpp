dde549c85c40cefc59faaa78255c3f5e71fcd9b2
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-05-28 23:45:40 +0600

    range based for loop

---
+++ Module6(classandstring)/range_based_for_loop.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+  string s;
+  cin>>s;
+  // index + element get
+  // for(int i = 0; i < s.size();i++)
+  // {
+  //   cout<<i<<"-"<<s[i]<<endl;
+  // }
+  // get element
+  for(char c:s)
+  {
+    cout<<c<<endl;
+  }
+  return 0;
+}
