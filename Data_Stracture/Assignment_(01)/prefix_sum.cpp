e41ede4c2e747ce0ebbc588c22022238b822dec0
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-06-09 17:04:45 +0600

    get prefix sum

--- Data_Stracture/Assignment_(01)/prefix_sum.cpp
+++ Data_Stracture/Assignment_(01)/prefix_sum.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 main()
 {
-  
+  long long int n;
+  cin>>n;
+  vector<int> a(n);
+  for(int i = 0; i < n; i++)
+  {
+    cin>>a[i];
+  }
+  long long int pre[n];
+  pre [0] = a[0];
+  for(int i = 1; i < n; i++){
+    pre[i]= a[i] + pre[i - 1];
+  }
+  for(int i = n - 1; i >= 0; i--){
+    cout<<pre[i]<<" ";
+  }
   return 0;
 }
