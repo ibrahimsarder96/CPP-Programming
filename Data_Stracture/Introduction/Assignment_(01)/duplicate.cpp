07968a90e8e22c287f81785fb907963f5063912b
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-06-11 22:05:25 +0600

    reference_pointer

--- Data_Stracture/Introduction/Assignment_(01)/duplicate.cpp
+++ Data_Stracture/Introduction/Assignment_(01)/duplicate.cpp
@@ -10,11 +10,9 @@ main()
     cin>>a[i];
   }
    bool flag = false;
-  //  sort(a,a+n);
   for(int i = 0; i < n - 1; i++)
   {
    for(int j = i + 1; j < n; j++){
-    // cout<<a[i];
      if(a[i]==a[j]){
       flag=true;
    }
