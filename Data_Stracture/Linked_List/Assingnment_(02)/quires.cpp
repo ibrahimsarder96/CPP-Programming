01bf75e93052caef0d73c1b5e14471c2e60e4b0d
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-07-13 22:20:47 +0600

    doubly linked list

--- Data_Stracture/Linked_List/Assingnment_(02)/quires.cpp
+++ Data_Stracture/Linked_List/Assingnment_(02)/quires.cpp
@@ -10,62 +10,60 @@ class Node{
         }
 };
 // Insert any position---------------
-// void  insert_at_position(Node* head, int pos, int v){
-//     Node* newNode = new Node(v);
-//     Node* tmp = head;
-//     for(int i = 1; i < pos - 1; i++){
-//       tmp = tmp -> next;
-//         return;
-//       }
-//     newNode->next = tmp->next;
-//     tmp->next = newNode;
-// }
-// // insert head and tail
-void insert_head_tail(Node *&head, Node* &tail, int v){
-      Node* newNode = new Node(v);
-      if(head == NULL){
-        head = newNode;
-        tail = newNode;
-        cout<<head<<" "<<tail;
+void  insert_at_position(Node* head, int pos, int v){
+    Node* newNode = new Node(v);
+    Node* tmp = head;
+    for(int i = 1; i < pos - 1; i++){
+      cout<<tmp->val;
+      tmp = tmp -> next;
         return;
       }
-      tail->next = newNode;
-      tail = newNode;
+    newNode->next = tmp->next;
+    tmp->next = newNode;
 }
+// insert head and tail
+// void insert_head_tail(Node *head, Node* tail, int v){
+//       Node* newNode = new Node(v);
+//       if(head == NULL){
+//         head = newNode;
+//         tail = newNode;
+//         cout<<head->val<<" "<<tail->val<<endl;
+//         return;
+//       }
+//       tail->next = newNode;
+//       tail = newNode;
+// }
 // // print function
-void print_linked_list(Node* &head){
-  Node* tmp = head;
-  while(tmp != NULL){
-    cout<<tmp->val<<" "<<tmp->next->val;
-    tmp=tmp->next;
-  }
-  cout<<endl;
-}
+// void print_linked_list(Node* &head){
+//   Node* tmp = head;
+//   while(tmp != NULL){
+//     cout<<tmp->val<<" "<<tmp->next->val;
+//     tmp=tmp->next;
+//   }
+//   cout<<endl;
+// }
+// void insert (int p,int val){
+//   cout<<p<<" "<<val<<endl;
+// }
 
 main()
 {
   Node* head = NULL;
   Node* tail = NULL; 
-  int n;
-  cin>>n;
-  for(int i =  0; i <= n; i++){
-   int pos;
-   cin>>pos;
-   if(pos == 0){
-    int v;
-    cin>>v;
-    cout<<pos<<" "<<v;
-        // insert_at_position(head, pos, v);
-        insert_head_tail(head, tail, v);
-   }
-   else if(pos == 1){
-    int v;
-    cin>>v;
-    cout<<pos<<" "<<v;
-    // insert_at_position(head, pos, v);
-    insert_head_tail(head, tail, v);
-   }
-    print_linked_list(head);
+  int q;
+  cin>>q;
+  while(q--){
+    int p, va;
+    cin>>p>>va;
+    if(p==0){
+     insert_at_position(head,p, va);
+    //  print_linked_list(head);
+    }
+    if(p==1){
+     insert_at_position(head,p, va);
+    //  print_linked_list(head);
+    }
+    
   }
 
   return 0;
