add7413aabdbd9ab49dcda06582e07a17dfc7746
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-07-13 23:58:38 +0600

    doubly linked list at postion

---
+++ Data_Stracture/Doubly_Linked_List/insert_at_position.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+  public:
+        int val;
+        Node* prev;
+        Node* next;
+        Node(int val){
+          this->val=val;
+          this-> prev = NULL;
+          this-> next = NULL;
+        }
+};
+void insert_normal(Node* head){
+  Node* tmp = head;
+  while(tmp != NULL){
+    cout<<tmp->val<<" ";
+    tmp=tmp->next;
+  }
+  cout<<endl;
+}
+void insert_reverse(Node* tail){
+  Node* tmp = tail;
+  while(tmp != NULL){
+    cout<<tmp->val<<" ";
+    tmp=tmp->prev;
+  }
+  cout<<endl;
+}
+void insert_at_position(Node* head, int pos, int val)
+{
+  Node *newNode = new Node(val);
+  Node* tmp = head;
+  for(int i = 0; i < pos; i++){
+    tmp = tmp->next;
+  }
+  newNode->next = tmp->next;  // 100 -> 30
+  tmp->next = newNode;  // 20 -> 100
+  newNode->next->prev = newNode; // 100 <- 30
+  newNode->prev = tmp; // 20 <- 100
+}
+main()
+{
+  Node* head = new Node(10);
+  Node* a = new Node(20);
+  Node* b = new Node(30);
+  Node* c = new Node(40);
+  Node* tail = c;
+
+  // connection----------
+  head->next=a;
+  a->prev = head;
+  a->next = b;
+  b->prev = a;
+  b->next = c;
+  c->prev = b;
+  insert_at_position(head, 1, 100);
+  insert_normal(head);
+  insert_reverse(tail);
+  return 0;
+}
